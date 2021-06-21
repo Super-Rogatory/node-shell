4f8f3cc100995629c3246ed8ec230bb2e8a94b55
ChrisNieves <nievescs20@gmail.com>
2021-06-21 14:33:19 -0400

    build in modules and ls

--- bash.js
+++ bash.js
@@ -1,3 +1,6 @@
+const pwd = require("./pwd");
+const ls = require("./ls");
+
 // Output a promt
 process.stdout.write("prompt > ");
 
@@ -5,7 +8,10 @@ process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
-    process.stdout.write(process.cwd());
+    pwd();
+  }
+  if (cmd === "ls") {
+    ls();
   }
   process.stdout.write("\nprompt > ");
 });
---
+++ pwd.js
@@ -0,0 +1,4 @@
+module.exports = function () {
+  // Output a promt
+  process.stdout.write(process.cwd());
+};
